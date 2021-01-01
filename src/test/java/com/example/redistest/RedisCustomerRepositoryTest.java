87ef344b629915eb726253556b441309f4bcedd2
johnwmcnay <john.w.mcnay@gmail.com>
2020-12-31 23:01:29 -0600

    made tests atomic and for any test data to be removed by the end

--- src/test/java/com/example/redistest/RedisCustomerRepositoryTest.java
+++ src/test/java/com/example/redistest/RedisCustomerRepositoryTest.java
@@ -3,9 +3,7 @@ package com.example.redistest;
 import com.example.redistest.models.Account;
 import com.example.redistest.models.Customer;
 import com.example.redistest.repos.CustomerRepository;
-import org.junit.Assert;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +12,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataRedisTest
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RedisCustomerRepositoryTest {
 
     @Autowired
     CustomerRepository repository;
 
-    @Test
-    public void testAdd() {
+    @Before
+    public void setup() {
         Customer customer = new Customer("80010121098", "John Smith");
         customer.addAccount(new Account("1234567890", 2000));
         customer.addAccount(new Account("1234567891", 4000));
         customer.addAccount(new Account("1234567892", 6000));
-        customer = repository.save(customer);
-        Assert.assertNotNull(customer);
+        repository.save(customer);
+    }
+
+    @After
+    public void after() {
+        repository.delete(repository.findByExternalId("80010121098"));
+        Assert.assertNull(repository.findByExternalId("80010121098"));
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertNotNull(repository.findByExternalId("80010121098"));
     }
 
     @Test
@@ -36,4 +43,5 @@ public class RedisCustomerRepositoryTest {
         Assert.assertNotNull(customer);
         Assert.assertEquals("80010121098", customer.getExternalId());
     }
+
 }
